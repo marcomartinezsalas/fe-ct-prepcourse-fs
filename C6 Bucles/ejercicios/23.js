84e85eccd4ee77f41ccb2ac3baf8f66f3b48ece3
marco <durodedomar2008@gmail.com>
2024-08-07 16:00:23 -0300

    c6 terminado

--- C6 Bucles/ejercicios/23.js
+++ C6 Bucles/ejercicios/23.js
@@ -6,6 +6,22 @@ function esNumeroPrimo(numero) {
   // IMPORTANTE: Recuerda que un número primo es aquel que
   // solo es divisible por sí mismo y por 1.
   // Tu código:
+  if (numero <= 1){
+    return false;
+  }
+  if ( numero === 2){
+    return true;
+  }
+  if (numero % 2 === 0){
+    return false;
+  }
+  
+  for (let i = 3; i <= Math.sqrt(numero); i += 2){
+    if (numero % i === 0){
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = esNumeroPrimo;

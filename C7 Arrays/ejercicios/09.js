2300c616bb22757b4b7ac83885c7ea0c5f5be060
marco <durodedomar2008@gmail.com>
2024-08-11 18:02:04 -0300

    pequeno cambio en m7t9

--- C7 Arrays/ejercicios/09.js
+++ C7 Arrays/ejercicios/09.js
@@ -2,7 +2,7 @@ function obtenerElementoAleatorio(array) {
    // Devuelve un elemento aleatorio del arreglo array.
    // PISTA: Usa el método Math.random().
    // Tu código:
-   let indicexd = Math.floor(Math.random() * array.length);
+   let indicexd = Math.round(Math.random() * array.length);
 
    return array[indicexd];
 }
